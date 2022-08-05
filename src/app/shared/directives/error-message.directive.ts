43d522ecf6eb634b0192713e80a848cea5ef37b7
rafikidota <lesmes240698@gmail.com>
2022-08-05 04:26:23 -0400

    video 8

--- src/app/shared/directives/error-message.directive.ts
+++ src/app/shared/directives/error-message.directive.ts
@@ -7,15 +7,21 @@ export class ErrorMessageDirective implements OnInit {
 
   htmlElement: ElementRef<HTMLElement>;
   @Input() color: string = 'red';
+  @Input() message: string = 'Este campo es requerido';
 
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = el;
   }
   ngOnInit(): void {
     this.setColor();
+    this.setMessage();
   }
   setColor(): void {
     this.htmlElement.nativeElement.style.color = this.color;
+    this.htmlElement.nativeElement.classList.add('form-text');
+  }
+  setMessage(): void {
+    this.htmlElement.nativeElement.innerText = this.message;
   }
 
 }
